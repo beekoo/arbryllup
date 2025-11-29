91cd649c46ad4fe5110341384c06f8d6e208a6da
Brendan Kennedy <beekoo.fc@gmail.com>
2025-11-29 17:19:15 +0100

    App: Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import './styles/App.css';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import SchedulePage from './pages/SchedulePage';
+import PlaceStayPage from './pages/PlaceStayPage';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const [imageSrc, setImageSrc] = useState('/img/tre.jpeg');
+
+  useEffect(() => {
+    const updateImage = () => {
+      if (window.innerWidth <= 768) {
+        setImageSrc('/img/en.jpeg');
+      } else {
+        setImageSrc('/img/tre.jpeg');
+      }
+    };
+
+    
+    updateImage();
+
+    window.addEventListener('resize', updateImage);
+
+    
+    return () => window.removeEventListener('resize', updateImage);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <div className='main-container'>
+          
+          <section className="left-sec">
+            <img src={imageSrc} alt="couple" className="weddingImg" />
+            <div className="left-text">
+              <h1>Aslak & Ragnhild</h1>
+              <p>Me gifte oss!</p>
+            </div>
+          </section>
+
+          <section className='right-sec'>
+            <Routes>
+              <Route path="/" element={<HomePage isLargeScreen={isLargeScreen} />} />
+              <Route path="/schedule" element={<SchedulePage isLargeScreen={isLargeScreen} />} />
+              <Route path="/placestay" element={<PlaceStayPage isLargeScreen={isLargeScreen} />} />
+
+            </Routes>
+          </section>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    </Router>
   )
 }
 
-export default App
+export default App;
