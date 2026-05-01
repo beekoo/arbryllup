0bd43e6fec3bd74b8240383dcf45f181cced7df0
Brendan Kennedy <beekoo.fc@gmail.com>
2026-05-01 09:48:24 +0200

    Update: QR code added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -41,7 +41,7 @@ const [timeLeft, setTimeLeft] = useState('');
           {/* <p className="countdown">{timeLeft}</p> */}
           <h2>Velkomen til vår bryllupshelg i Hardanger!</h2>
           {/* <p>Me gler oss til å feira bryllupet vårt med familie og venar <br/> 1. mai helga 2026!</p> */}
-          <img src="../public/img/qr_code.JPG" alt="" />
+          <img src="/img/qr_code.JPG" alt="" />
           {/* <p>Her finn du informasjon om bryllupshelga. I dei andre fanene (på mobil tre strekar oppe i høgre hjørne) finn du nærare program, nærare informasjon om stad, opphald og lenke til ynskjeliste.</p> */}
           {/* <p>  <a href="https://docs.google.com/forms/d/1szIkqMf9pyR5LlJcMOPnxfGY4FOE6dEFqpLbmjeCSk0/viewform?edit_requested=true">invitasjonsskjema</a> så fort som mogleg og seinast innan 1. februar 2026.</p> */}
           
